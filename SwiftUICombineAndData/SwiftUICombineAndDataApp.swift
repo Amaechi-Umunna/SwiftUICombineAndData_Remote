4d0066374ca03975a36315d4c43c5e9c191c895b
Umunnabuike <100558243+Umunnabuike@users.noreply.github.com>
2022-05-21 09:49:26 +0100

    Initial Commit

---
+++ SwiftUICombineAndData/SwiftUICombineAndDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineAndDataApp.swift
+//  SwiftUICombineAndData
+//
+//  Created by Amaechi Chukwu on 21/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineAndDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
