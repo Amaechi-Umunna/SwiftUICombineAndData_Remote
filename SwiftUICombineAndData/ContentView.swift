f5e09862130027b47cbbe87cda3b4e528c1dc812
Umunnabuike <100558243+Umunnabuike@users.noreply.github.com>
2022-05-27 16:23:12 +0100

    Worked on GradientIcon. Created FAQView and linked it through Navigation link, Packages View and the YouTube channel which was linked using "Link" instead of Navigation Link. Created groups too to better curate the growing number of files.

--- SwiftUICombineAndData/ContentView.swift
+++ SwiftUICombineAndData/ContentView.swift
@@ -8,14 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var contentOffset = CGFloat(0)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack(alignment: .top) {
+                TrackableScrollView(offsetChanged: {
+                    offset in
+                    contentOffset = offset.y
+                    
+                }) {
+                content
+                    
+                }
+                VisualEffectBlur(blurStyle: .systemMaterial)
+                    .opacity(contentOffset < -16 ? 1 : 0)
+                    .animation(.easeIn)
+                    .ignoresSafeArea()
+                    .frame(height: 0)
+            }
+            .frame(maxHeight: .infinity, alignment: .top)
+            .background(AccountBackground())
+            .navigationBarHidden(true)
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+    
+    var content: some View {
+        VStack {
+//            More Content
+            VStack {
+                NavigationLink(destination: FAQView()) {
+                    MenuRow()
+                }
+                
+                divider
+                
+                NavigationLink(destination: PackagesView()) {
+                    MenuRow(title: "SwiftUI Packages", leftIcon: "square.stack.3d.up.fill")
+                }
+                
+                divider
+                
+                Link(destination: URL(string: "https://www.youtube.com/c/DesignCodeTeam")!, label: {
+                    MenuRow(title: "YouTube Channel", leftIcon: "play.rectangle.fill", rightIcon: "link")
+                })
+                
+            }
+            .padding(16)
+            .background(Color("Background 1"))
+            .background(VisualEffectBlur(blurStyle: .systemUltraThinMaterialDark))
+            .overlay(RoundedRectangle(cornerRadius: 20, style: .continuous).stroke(Color.white, lineWidth: 1).blendMode(.overlay))
+            .mask(RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .padding(.top, 20)
+            
+            Text("Version 1.00")
+                .foregroundColor(.white.opacity(0.7))
+                .padding(.top, 20)
+                .padding(.horizontal, 20)
+                .padding(.bottom, 20)
+                .font(.footnote)
+        }
+        .foregroundColor(.white)
+        .padding(.top, 20)
+        .padding(.horizontal, 20)
+        .padding(.bottom, 10)
+        
+    }
+    
+    var divider: some View {
+        Divider().background(Color.white.blendMode(.overlay))
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+//            .preferredColorScheme(.dark)
     }
 }
