88899d6037a261006b18b2aa7be38fef14f886d9
Umunnabuike <100558243+Umunnabuike@users.noreply.github.com>
2022-06-04 13:03:54 +0100

    Added the app to Firestore and aligned it so it sourced it's data from Firestore/Firebase, then created the certificate view model and the certificates view.

---
+++ SwiftUICombineAndData/Models/Certificate.swift
@@ -0,0 +1,16 @@
+//
+//  Certificate.swift
+//  SwiftUICombineAndData
+//
+//  Created by Amaechi Chukwu on 03/06/2022.
+//
+
+import Foundation
+
+struct Certificate: Identifiable {
+    var id: String
+    var course: String
+    var date: String
+    var logo: String
+    var instructor: String
+}
